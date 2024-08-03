c023fb3a291feda02a0a55c5a3ae869e0b3a0ecd
bakuncwa <gy.almirol@gmail.com>
2024-08-03 22:49:22 +0800

    Initial Tables Set Up

---
+++ Bayanihand.DataModel/CheckInINV.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bayanihand.DataModel
+{
+    public class CheckInINV
+    {
+        public int CheckInID;
+        public DateTime DateCheckedIn {  get; set; }
+        public DateTime? DateCheckedOut { get; set; }
+    }
+}
---
+++ Bayanihand.DataModel/ForumINV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bayanihand.DataModel
+{
+    public class ForumINV
+    {
+        public int ForumPostID { get; set; }
+        public string Title { get; set; }
+        public string? DateCheckedOut { get; set; } = string.Empty;
+        public DateTime DatePosted { get; set; }
+        public DateTime DateEdited { get; set; }
+
+        public string Status { get; set; }
+    }
+}
