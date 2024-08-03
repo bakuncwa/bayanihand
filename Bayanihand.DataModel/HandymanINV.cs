ecd7dde5241493b68166abf10f9753ec3e8f0059
bakuncwa <gy.almirol@gmail.com>
2024-08-03 22:52:07 +0800

    Reapply "Initial Tables Set Up"
    This reverts commit 2bafd1770270a9da75069bb68026f9f30b1a78bc.

---
+++ Bayanihand.DataModel/HandymanINV.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bayanihand.DataModel
+{
+    public class HandymanINV
+    {
+        public int HandymanID { get; set; }
+        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string ContactNo { get; set; } = string.Empty;
+        public string Email { get; set;} = string.Empty;
+        public string GovID { get; set; } = string.Empty;
+        public string ProfilePhoto {  get; set; } = string.Empty;
+        public string HandymanESign { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string BarangayNo { get; set; } = string.Empty;
+        public string BarangayName { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string ZIPCode { get; set; } = string.Empty;
+
+    }
+}
