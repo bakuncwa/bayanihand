5961b9e4cd72a0c205fd0f2ffb8f1e67d219017f
Kurt Hacinas <kurtandrei.hacinas@benilde.edu.ph>
2024-08-08 00:16:46 +0800

    Added admin controller and views (still no proper ID and action). Fixed the navbar.

---
+++ Bayanihand.App/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Bayanihand.DataModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bayanihand.App.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Customer()
+        {
+            //populating data for checking
+            var customerINQ = new InquiryINV
+            {
+                DateInquired = DateTime.Now,
+                Title = "Bad Handyman",
+                Description = "Construction Worker",
+            };
+            var customerList = new List<InquiryINV> { customerINQ };
+
+            return View(customerList);
+        }
+
+        public IActionResult Handyman()
+        {
+            //populating data for checking
+            var handymanINQ = new InquiryINV
+            {
+                DateInquired = DateTime.Now,
+                Title = "Bad Customer",
+                Description = "Construction Worker",
+            };
+            var handymanList = new List<InquiryINV> { handymanINQ };
+
+            return View(handymanList);
+        }
+
+    }
+}
