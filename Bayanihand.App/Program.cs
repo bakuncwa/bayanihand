9677a48cd6f4af4fbe830a21ca6c982c4e6b1dd5
bakuncwa <gy.almirol@gmail.com>
2024-08-07 21:40:08 +0800

    Configured Program.cs to accommodate IdentityRoles

--- Bayanihand.App/Program.cs
+++ Bayanihand.App/Program.cs
@@ -3,67 +3,77 @@ using Bayanihand.DataModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-//Service to use AppDbContext
-builder.Services.AddDbContext<AppDbContext>(opts =>
+namespace Bayanihand.App
 {
-    opts.UseSqlServer(builder.Configuration.GetConnectionString("Almirol"));
-});
-
-//Service to use Automapper
-builder.Services.AddAutoMapper(typeof(AutomapperConfig));
-
-//Add service for Microsoft Identity
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    public class Program
     {
-        //Configure authentication requirements
-        options.SignIn.RequireConfirmedEmail = false;
-        options.SignIn.RequireConfirmedPhoneNumber = false;
-        options.Password.RequireDigit = true;
-        options.Password.RequiredLength = 5;
-        options.Password.RequireLowercase = true;
-        options.Password.RequireUppercase = true;
-        options.Password.RequireNonAlphanumeric = true;
-    }).AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddSession();
+            //Service to use AppDbContext
+            builder.Services.AddDbContext<AppDbContext>(opts =>
+            {
+                opts.UseSqlServer(builder.Configuration.GetConnectionString("Almirol"));
+            });
 
-var app = builder.Build();
+            //Service to use Automapper
+            builder.Services.AddAutoMapper(typeof(AutomapperConfig));
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Landing/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+            //Add service for Microsoft Identity
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                //Configure authentication requirements
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+            }).AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseSession();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddSession();
 
-app.UseRouting();
+            var app = builder.Build();
 
-app.UseAuthorization();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Landing/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Landing}/{action=Index}/{id?}");
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
 
-using (var scope = app.Services.CreateScope()) 
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            app.UseRouting();
 
-    var roles = new[] { "Admin" };
+            app.UseAuthorization();
 
-    foreach(var r in roles) 
-    {
-        if (! await roleManager.RoleExistsAsync(r))
-            await roleManager.CreateAsync(new IdentityRole(r));
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Landing}/{action=Index}/{id?}");
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                var roles = new[] { "Admin" };
+
+                foreach (var r in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(r))
+                        await roleManager.CreateAsync(new IdentityRole(r));
+                }
+            }
+
+            app.Run();
+        }
     }
 }
 
-    app.Run();
