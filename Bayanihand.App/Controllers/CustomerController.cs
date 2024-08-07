5dfe3ba10bc10389093582451f0b4eab77c29ef1
Kurt Hacinas <kurtandrei.hacinas@benilde.edu.ph>
2024-08-07 21:40:35 +0800

    Added the table for Handy Applicants view, action in controller and navbar. Will finish once identity tables are done.

--- Bayanihand.App/Controllers/CustomerController.cs
+++ Bayanihand.App/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bayanihand.DataModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bayanihand.App.Controllers
 {
@@ -7,6 +8,23 @@ namespace Bayanihand.App.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Applicants()
+        {
+            //populating data for checking
+            //var applicant = new ApplicationINV
+            //{
+            //    ApplicationID = 1,
+            //    DateApplied = DateTime.Now,
+            //    Title = "Fix Kitchen",
+            //    CV = "Construction Worker",
+            //    ForumPostID = 1,
+            //    HandymanID = 1,
+            //};
+            //var applicantsList = new List<ApplicationINV> { applicant };
+
+            return View(new List<ApplicationINV>());
         }
     }
 }
