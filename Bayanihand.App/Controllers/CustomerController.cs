643401f0ad321ee765c74414ddaccac843555c32
Kurt Hacinas <kurtandrei.hacinas@benilde.edu.ph>
2024-08-08 15:36:50 +0800

    Added follow up view and changes in customer controller. Fixed repositories and services.

--- Bayanihand.App/Controllers/CustomerController.cs
+++ Bayanihand.App/Controllers/CustomerController.cs
@@ -133,18 +133,22 @@ namespace Bayanihand.App.Controllers
         public IActionResult Applicants()
         {
             //populating data for checking
-            //var applicant = new ApplicationINV
-            //{
-            //    ApplicationID = 1,
-            //    DateApplied = DateTime.Now,
-            //    Title = "Fix Kitchen",
-            //    CV = "Construction Worker",
-            //    ForumPostID = 1,
-            //    HandymanID = 1,
-            //};
-            //var applicantsList = new List<ApplicationINV> { applicant };
+            var applicant = new ApplicationINV
+            {
+                ApplicationID = 1,
+                DateApplied = DateTime.Now,
+                hasApplied = true,
+                ForumPostID = 1,
+                HandymanID = 1,
+            };
+            var applicantsList = new List<ApplicationINV> { applicant };
 
-            return View(new List<ApplicationINV>());
+            return View(applicantsList);
+        }
+
+        public IActionResult FollowUp()
+        {
+            return View();
         }
 
         public IActionResult JobSchedule()
